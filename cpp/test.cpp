f4b8a25ddb325cfaa9de1ebc1e2096f8d8f44382
Ankita04-kumari <ankitacoding04@gmail.com>
2023-08-16 23:07:41 +0530

    new project

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,1 +1,25 @@
-abcd
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    unordered_map <char, int> m ={ {'I',1} , {'V',5} , {'X',10} , {'L',50} , {'C',100} , {'D',500} , {'M',1000} };
+    int result = 0;
+    string str;
+    cin >> str;
+    int n = str.length();
+    for( int i=0 ;i < n ; i++ ){
+        if( m [ str[ i ] ]< m [ str [ i+1 ] ]){
+            result -= m[ str[i] ];
+        } else {
+            result += m[ str[i] ];
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
